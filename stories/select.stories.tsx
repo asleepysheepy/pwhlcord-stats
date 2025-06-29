b981591228e2ffbb13056887366242fb300a9e28
Katie Macke <katie@sleepysheepy.dev>
2025-06-29 11:54:24 -0400

    add table and select components

---
+++ stories/select.stories.tsx
@@ -0,0 +1,55 @@
+import { type Meta, type StoryObj } from '@storybook/react-vite'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select'
+
+const meta = {
+  title: 'UI/Select',
+  parameters: {
+    layout: 'centered',
+  },
+  component: Select,
+  subcomponents: {
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectScrollDownButton,
+    SelectScrollUpButton,
+    SelectSeparator,
+    SelectTrigger,
+    SelectValue,
+  },
+} satisfies Meta<typeof Select>
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: () => (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+      <SelectGroup>
+        <SelectLabel>Fruits</SelectLabel>
+      <SelectItem value="apple">Apple</SelectItem>
+    <SelectItem value="banana">Banana</SelectItem>
+    <SelectItem value="blueberry">Blueberry</SelectItem>
+    <SelectItem value="grapes">Grapes</SelectItem>
+    <SelectItem value="pineapple">Pineapple</SelectItem>
+    </SelectGroup>
+    </SelectContent>
+    </Select>
+),
+}
