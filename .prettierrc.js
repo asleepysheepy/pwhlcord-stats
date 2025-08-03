5e19243b32f62fdbd09fcd4a5b1589977b76b1f7
Katie Macke <katie@sleepysheepy.dev>
2025-08-03 08:32:29 -0400

    bye bye react router

--- .prettierrc.js
+++
@@ -1,4 +0,0 @@
-import epicConfig from '@epic-web/config/prettier'
-
-/** @type {import("prettier").Options} */
-export default { ...epicConfig, useTabs: false, printWidth: 100 }
--- app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes'
-
-export default [
-  index('routes/home/home.tsx'),
-  ...prefix('resources', [route('team-logo/:imageId', 'routes/resources/team-logo.tsx')]),
-] satisfies RouteConfig
