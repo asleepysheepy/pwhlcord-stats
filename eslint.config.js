9c4e9898f0786b9f879d325fd04adedf743fa77a
Katie Macke <katie@sleepysheepy.dev>
2025-08-13 23:43:59 -0400

    eslint import sorting

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
-import storybook from 'eslint-plugin-storybook'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import { includeIgnoreFile } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
+import storybook from 'eslint-plugin-storybook'
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url))
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -13,6 +16,26 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   ...storybook.configs['flat/recommended'],
+  includeIgnoreFile(gitignorePath),
+  {
+    rules: {
+      'import/order': [
+          'warn',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          pathGroups: [{ pattern: '#*/**', group: 'internal' }],
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+        },
+      ]
+    }
+  }
 ]
 
 export default eslintConfig
