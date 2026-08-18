49e55975af5b8c41bd33bd4a97aac512da5fc1ee
Katie Macke <katie@sleepysheepy.dev>
2026-08-18 09:36:19 -0400

    Dockerize

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,8 @@
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
 
 const isTest = process.env.NODE_ENV === 'test'
 const databaseUrl = process.env.DATABASE_URL
 const useMock = isTest || databaseUrl == null
 
-export const db = useMock ? drizzle.mock({ schema }) : drizzle({ client: neon(process.env.DATABASE_URL!), schema })
+export const db = useMock ? drizzle.mock({ schema }) : drizzle({ connection: process.env.DATABASE_URL!, schema })
